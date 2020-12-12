e0505c362c24106e07ee3a8d92c8a664b2976e3e
Francielly Amparo do Santos Vieira <francielly.asvieira@gmail.com>
2020-12-12 19:23:57 -0300

    alteracoes

---
+++ js/main.js
@@ -0,0 +1,115 @@
+
+
+var idade = prompt("Qual sua idade?");
+console.log(validarIdade(idade));
+
+function validarIdade(idade){
+	var validar;
+	if (idade >= 18){
+		alert("Vamos prosseguir?");
+		return true;
+	}else{
+		alert("Página inapropriada para a sua idade!");
+		window.location.href = scr="index.html";
+	}
+	
+}
+
+
+function formulario(){
+	alert("teste");
+}
+
+function clicou (){
+	document.getElementById
+}
+
+	
+function cadastro(){
+	var dadosP =["1 Leticia","2 Alice"];
+	for (var count = 0; count < dadosP.length; count++){
+	console.log(dadosP[count]);	
+	document.getElementById(dadosP);
+	}
+	
+	
+
+}
+
+//console.log(validarIdade(idade));
+
+/*
+function clicou (){
+	alert("obrigado por clicar");
+}
+*/
+
+/*function agradecer(){
+	alert ("Precisamos de algumas confirmações antes de prosseguir");
+}*;
+/*
+function soma(n1,n2){
+return n1 + n2;
+}
+
+function setReplace(frase, nome, novo_nome){
+	return frase.replace(nome, novo_nome)
+}
+
+alert(setReplace("Vai Japão", "Japão", "Brasil"));
+alert(soma(5, 10));
+*/
+//var d = new date();
+
+
+
+/*
+var count;
+for(count=0; count<=5; count++){
+
+}
+
+*/
+/*
+var count = 5;
+while(count<5){
+	console.log(count);
+	count++;
+
+*/
+/*
+var idade = prompt("Qual é a sua idade?");
+function validarIdade(idade){
+if (idade >= 18){
+		alert("maior de idade");
+
+}else{
+	alert9"menor de idade");
+}
+}
+*/
+
+/*
+var cadastro = [{nome:"Marcos", cidade:"Recife"},{nome:"Anderson", cidade:"Jaboatão"});
+console(cadastro);
+alert(cadastro[1].nome);
+*/
+/*
+var cadastro = {nome:"maça", cor:"vermelho"}
+console.log(fruta.nome)/
+*/
+
+
+//var name = ".";
+//var idade = 28;
+////var idade2 = 10;
+
+//var lista = ["fran", "vieira", ]
+//lista.push("cpf"); //iserir na listar
+// lista.pop(); retira da lista
+
+//console.log(lista[0]);//tamanho da lista
+//console.log(lista.reverse());//
+//onsole.log(lista.Tostring()); para virar string
+//console.log(lista.join("/ ")); separar lista
+
